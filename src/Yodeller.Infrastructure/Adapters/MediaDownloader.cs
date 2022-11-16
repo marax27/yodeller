8684f5593365da0dfa81eca302ff9805be26c256
Kacper Tonia <kacpert314@gmail.com>
2022-11-17 00:02:49 +0100

    Send subtitle patterns from the frontend

--- src/Yodeller.Infrastructure/Adapters/MediaDownloader.cs
+++ src/Yodeller.Infrastructure/Adapters/MediaDownloader.cs
@@ -1,4 +1,5 @@
-﻿using Yodeller.Application.Downloader;
+﻿using System.Text;
+using Yodeller.Application.Downloader;
 using Yodeller.Application.Ports;
 using Yodeller.Infrastructure.Utilities;
 
@@ -8,14 +9,32 @@ public class YtDlpMediaDownloader : IMediaDownloader
 {
     public bool Download(DownloadProcessSpecification what)
     {
-        var arguments = what.AudioOnly
-            ? $"-f \"bestaudio/best\" -ciw -v --extract-audio --audio-quality 0 --audio-format mp3 {what.MediaLocator}"
-            : what.MediaLocator;
+        var arguments = CreateDownloaderArguments(what);
 
         var execution = new ProcessExecution();
 
         var result = execution.Run(new("yt-dlp", arguments, "/out")).Result;
 
         return result.ExitCode == 0;
+    }
+
+    private static string CreateDownloaderArguments(DownloadProcessSpecification what)
+    {
+        var argumentsBuilder = new StringBuilder();
+
+        if (what.AudioOnly)
+        {
+            argumentsBuilder.Append("-f \"bestaudio/best\" -ciw -v --extract-audio --audio-quality 0 --audio-format mp3 ");
+        }
+
+        argumentsBuilder.Append(what.MediaLocator);
+
+        if (what.SubtitlePatterns.Any())
+        {
+            var subtitleArgument = string.Join(",", what.SubtitlePatterns);
+            argumentsBuilder.Append($" --write-subs --sub-langs \"{subtitleArgument}\"");
+        }
+
+        return argumentsBuilder.ToString();
     }
 }
