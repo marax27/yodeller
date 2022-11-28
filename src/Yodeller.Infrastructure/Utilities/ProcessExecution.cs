e14621de8c0abe539a4140b56dec340d75c37d13
Kacper Tonia <kacpert314@gmail.com>
2022-11-28 20:49:32 +0100

    Drop obsolete code

--- src/Yodeller.Infrastructure/Utilities/ProcessExecution.cs
+++ src/Yodeller.Infrastructure/Utilities/ProcessExecution.cs
@@ -40,30 +40,4 @@ internal class ProcessExecution
 
         return result;
     }
-
-    public async ValueTask<Result> RunObsolete(ProcessParameters parameters)
-    {
-        var process = new Process()
-        {
-            StartInfo = new()
-            {
-                FileName = parameters.ApplicationName,
-                Arguments = parameters.Arguments,
-                UseShellExecute = false,
-                CreateNoWindow = true,
-                RedirectStandardOutput = false
-            }
-        };
-
-        if (parameters.WorkingDirectory is not null)
-        {
-            process.StartInfo.WorkingDirectory = parameters.WorkingDirectory;
-        }
-
-        process.Start();
-
-        await process.WaitForExitAsync();
-
-        return new Result(process.ExitCode);
-    }
 }
