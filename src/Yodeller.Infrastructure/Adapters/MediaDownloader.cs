39b4d1815a4db09dbaf246c561ee7b16cd24061e
Kacper Tonia <kacpert314@gmail.com>
2022-11-28 20:18:06 +0100

    Add CliWrap to process execution

--- src/Yodeller.Infrastructure/Adapters/MediaDownloader.cs
+++ src/Yodeller.Infrastructure/Adapters/MediaDownloader.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using Microsoft.Extensions.Logging;
 using Yodeller.Application.Downloader;
 using Yodeller.Application.Ports;
 using Yodeller.Infrastructure.Utilities;
@@ -7,13 +8,40 @@ namespace Yodeller.Infrastructure.Adapters;
 
 public class YtDlpMediaDownloader : IMediaDownloader
 {
+    private readonly ILogger<YtDlpMediaDownloader> _logger;
+
+    public YtDlpMediaDownloader(ILogger<YtDlpMediaDownloader> logger)
+    {
+        _logger = logger;
+    }
+
     public bool Download(DownloadProcessSpecification what)
     {
         var arguments = CreateDownloaderArguments(what);
 
         var execution = new ProcessExecution();
 
-        var result = execution.Run(new("yt-dlp", arguments, "/out")).Result;
+        var foundErrors = false;
+        var errorOutputBuilder = new StringBuilder();
+
+        var result = execution.Run(
+            new(
+                "yt-dlp",
+                arguments,
+                stdout => { },
+                stderr =>
+                {
+                    if (!foundErrors)
+                        _logger.LogError("Errors during download... Full log will be emitted after the sub-process exits.");
+                    foundErrors = true;
+                    errorOutputBuilder.AppendLine(stderr);
+                },
+                "/out"
+            )
+        ).Result;
+
+        if (foundErrors)
+            _logger.LogError("Downloader error output: {ErrorOutput}", errorOutputBuilder.ToString());
 
         return result.ExitCode == 0;
     }
